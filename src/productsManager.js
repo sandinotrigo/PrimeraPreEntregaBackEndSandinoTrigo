843dce6dbbf8a1250a37d36eac8c05083aebdb49
sandinotrigo <sanditrimun123@gmail.com>
2023-03-04 23:49:03 -0300

    desafio1

--- src/productsManager.js
+++ src/productsManager.js
@@ -1,52 +1,59 @@
-import * as fs from 'fs';
-
 class ProductManager {
-  #path = "./Products.json";
-  #codeaccumulator= 0;
+  //se crea la clase Manager
+ //definimos productos como un arreglo vacio
 
-  async addProduct(title, description, price,thumbnail,code,stock) {
-    const newProduct = {
-        title, 
-        description,
-        price,
-        thumbnail,
-        code: this.#codeaccumulator, 
-        stock,
+  products = [];
+  #codeaccumulator = 0;
+
+  addProduct(title, description, price, thumbnail, id, stock) { //addProduct agregara el producto en el array products
+    
+    const nuevoProducto = { //propiedades a ingresar del producto
+      
+      title, 
+      description,
+      price,
+      thumbnail,
+      id: this.#codeaccumulator,
+      stock,
     };
 
-    const products = await this.getProducts();
+    const alreadyExists = this.products.some((p) => p.id === id);//se busca el id para retornarlo como valor booleano
 
-    const updatedUsers = [...products, newProduct];
-
-    await fs.promises.writeFile(this.#path, JSON.stringify(updatedUsers));
-  }
-
-  async getProducts() {
-    try {
-      const products = await fs.promises.readFile(this.#path, "utf-8");
-
-      return JSON.parse(products);
-    } catch (e) {
-      return [];
+    if (alreadyExists) {
+      throw new Error("Producto o codigo repetido " );
     }
+    if (!title || !description || !price ||!thumbnail||!stock) {
+      throw new Error("Error, datos erroneos");
+    }
+
+    this.#codeaccumulator++; //esto es lo mismo que poner +1
+    this.products = [...this.products, nuevoProducto];
   }
-  async hola() {
-    const manager = new ProductManager();
-  
-    console.log(await manager.getProducts());
-  
-    await manager.addProduct(
-      "Manzanas",
-      "verdes",
-      15,
-      "no disponible",
-      30
-    );
-  
-    console.log(await manager.getProducts());
+
+  getProducts() {
+    //Este metodo se encarga de retornar todos los productos
+    return this.products;
+  }
+
+  getProductsbyId(id){ //obtener solamente el producto que coincida su code con el id ingresada por el manager
+    const idIngresada = {id: this.#codeaccumulator,}
+                        
+    const code = this.#codeaccumulator;
+    
+    const buscarId = this.products.find( (p) => p.id === id); //se busca en el array products con find (some no porque arroja booleanos)
+      
+
+    
+
+    if(buscarId===idIngresada){
+      throw console.log("El producto es el siguiente" + buscarId); //Si el id ingresada coincide con lo que esta en code  se ejecuta
+    }
   }
 }
 
+const manager = new ProductManager();
 
+manager.addProduct("manzana", "fruta", 10, "imagen", "1", 10);
+manager.addProduct("banana","fruta" ,100,"no","2",10);
+console.log(manager.getProductsbyId(1));
 
-export default ProductManager
